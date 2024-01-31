3e2cac9ffa301a21f404b7132ec3385e92ea9a3c
anandraj <anandrajvishwanathan@gmail.com>
2024-01-31 21:39:49 +0530

    my file

---
+++ javascript/index.js
@@ -0,0 +1,36 @@
+//array//
+let arr = [1,2,3,4];
+
+console.log(arr);
+
+let x =2
+let y =5
+let z ="x+y";
+
+console.log(z);
+//itll delete the last num//
+  arr.push("true");
+  console.log(arr);
+  arr.pop();
+  console.log(arr);
+
+  //object//
+  var object = {name:"apple"};
+  console.log(object);
+
+  //identifiers- set of rules to create a variable//
+   
+
+  //dot notation//
+   let object2 = {performance:100,age:20};
+   console.log(object2.age);
+   console.log(object2.performance);
+    
+   //bracket notation//
+   let object3 = {price:100,quality:50};
+   console.log(object2[performance]);
+   console.log(object3[price]);
+
+   //non existing property accessed means itll show undefined//
+
+
